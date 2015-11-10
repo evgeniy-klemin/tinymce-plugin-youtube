3f433b970e5ab45217fefed11c875022b4614174
Dmitry Sukhovetsky <sukhovetsky@gmail.com>
2015-11-10 06:31:29 +0300

    fix lang

--- plugin.js
+++ plugin.js
@@ -20,6 +20,21 @@
  # -- END LICENSE BLOCK -----------------------------------
  */
 (function (tiny) {
+	
+	tinymce.addI18n('ru',{
+	    'YouTube Tooltip'   : "YouTube",
+		'YouTube Title'     : "Вставить видео YouTube",
+		'Youtube URL'	    : 'Ссылка',
+		'Youtube ID'        : 'Формат ссылки: http://youtu.be/xxxxxxxx или http://www.youtube.com/watch?v=xxxxxxxx',
+		'width'			    : 'Ширина',
+		'height'		    : 'Высота',
+		'autoplay'		    : 'Автозапуск',
+		'Related video'     : 'Похожие видео',
+		'HD video'          : 'В формате HD',
+	    'html5'             : 'HTML5',
+	    'Insert'            : 'Вставить'
+	});
+
     tiny.PluginManager.add("youtube", function (editor, url) {
     	var url = url.split('/').slice(0, -3).join('/');
 
